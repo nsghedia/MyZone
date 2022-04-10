6b5752ab9ff4f871abb091321cb53e6e21285204
Neha Ghedia <nsghedia@gmail.com>
2022-04-11 01:42:34 +0530

    Custom Validators [User Register] - part-3

--- src/app/user/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register.component.ts
@@ -11,13 +11,34 @@ export class UserRegisterComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    this.RegisterForm = new FormGroup({
-      userName: new FormControl('Neha Ghedia', Validators.required),
-      email: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required),
-      cpassword: new FormControl('', Validators.required),
-      mobile: new FormControl('', Validators.required),
-    });
+    this.RegisterForm = new FormGroup(
+      {
+        userName: new FormControl('Neha Ghedia', Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.maxLength(15),
+          Validators.minLength(8),
+        ]),
+        cpassword: new FormControl('', [Validators.required]),
+        mobile: new FormControl('', [
+          Validators.required,
+          Validators.maxLength(15),
+          Validators.minLength(9),
+        ]),
+      },
+      this.passwordMatchingValidator(this.RegisterForm)
+    );
+  }
+
+  passwordMatchingValidator(formGroup: FormGroup): Validators | null {
+    if (!formGroup) {
+      return null;
+    }
+    return formGroup.get('password')?.value ===
+      formGroup.get('cpassword')?.value
+      ? null
+      : { notMatched: true };
   }
 
   onSubmit() {
