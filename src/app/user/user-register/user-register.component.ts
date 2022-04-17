1320c86c3acebe2d1269b002c79331b6bde3b93a
Neha Ghedia <nsghedia@gmail.com>
2022-04-17 15:43:12 +0530

    working more on FormGroup

--- src/app/user/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
@@ -8,26 +13,62 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class UserRegisterComponent implements OnInit {
   RegisterForm!: FormGroup;
-  constructor() {}
+  user: any = {};
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.RegisterForm = new FormGroup(
+    // this.RegisterForm = new FormGroup(
+    //   {
+    //     userName: new FormControl('Neha Ghedia', Validators.required),
+    //     email: new FormControl(null, [Validators.required, Validators.email]),
+    //     password: new FormControl('', [
+    //       Validators.required,
+    //       Validators.maxLength(15),
+    //       Validators.minLength(8),
+    //     ]),
+    //     cpassword: new FormControl('', [Validators.required]),
+    //     mobile: new FormControl('', [
+    //       Validators.required,
+    //       Validators.maxLength(15),
+    //       Validators.minLength(9),
+    //     ]),
+    //   },
+    //   this.passwordMatchingValidator(this.RegisterForm)
+    // );
+    this.createRegisterationForm();
+  }
+
+  createRegisterationForm() {
+    this.RegisterForm = this.fb.group(
       {
-        userName: new FormControl('Neha Ghedia', Validators.required),
-        email: new FormControl(null, [Validators.required, Validators.email]),
-        password: new FormControl('', [
-          Validators.required,
-          Validators.maxLength(15),
-          Validators.minLength(8),
-        ]),
-        cpassword: new FormControl('', [Validators.required]),
-        mobile: new FormControl('', [
-          Validators.required,
-          Validators.maxLength(15),
-          Validators.minLength(9),
-        ]),
+        userName: [null, Validators.required],
+        email: [null, Validators.required, Validators.email],
+        password: [
+          null,
+          [
+            Validators.required,
+            Validators.maxLength(15),
+            Validators.minLength(8),
+          ],
+        ],
+        cpassword: [
+          null,
+          [
+            Validators.required,
+            Validators.maxLength(15),
+            Validators.minLength(8),
+          ],
+        ],
+        mobile: [
+          null,
+          [
+            Validators.required,
+            Validators.maxLength(15),
+            Validators.minLength(9),
+          ],
+        ],
       },
-      this.passwordMatchingValidator(this.RegisterForm)
+      { Validators: this.passwordMatchingValidator }
     );
   }
 
@@ -43,5 +84,28 @@ export class UserRegisterComponent implements OnInit {
 
   onSubmit() {
     console.log(this.RegisterForm);
+    this.user = Object.assign(this.user, this.RegisterForm.value);
+    // localStorage.setItem("User",JSON.stringify(this.user));
+    this.addUsers(this.user);
+  }
+
+  Compare() {
+    if (
+      this.RegisterForm.get('password')?.value ===
+      this.RegisterForm.get('cpassword')?.value
+    ) {
+      console.log('Matched');
+    }
+  }
+
+  addUsers(user: any) {
+    let users = [];
+    if (localStorage.getItem('User')) {
+      users = JSON.parse(localStorage.getItem('User') || '{}');
+      users = [user, ...users];
+    } else{
+      users = [user];
+    }
+     localStorage.setItem('User', JSON.stringify(users));
   }
 }
